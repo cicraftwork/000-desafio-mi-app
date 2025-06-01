acd500eac430d884ede6d30d649fcdf88f2daca8
cicraftwork <ci.nuclear@gmail.com>
2025-06-01 18:53:37 -0400

    4

--- server.js
+++ server.js
@@ -5,4 +5,4 @@ const app = express()
 
 const PORT = process.env.PORT || 3000
 
-app.listen(PORT, console.log('server on http://localhost:${PORT}'))
+app.listen(PORT, console.log('Server on http://localhost:${PORT}'))
